d34c76dd6e230b1570b67a9357480f70ac0104d4
Gajanan Mallik <gajanan.mallik@tdcx.com>
2022-06-11 23:23:31 +0530

    use memo

---
+++ src/UseMomo/Memo.js
@@ -0,0 +1,35 @@
+import React, { Component, useMemo, useState } from 'react';
+
+function Memo(props) {
+
+    const [counterOne, setCounterOne] = useState(0);
+    const [counterTwo, setCounterTwo] = useState(0);
+
+    const incrementOne = () => {
+        setCounterOne(counterOne + 1);
+    }
+
+    const incrementTwo = () => {
+        setCounterTwo(counterTwo + 1);
+    }
+
+    const isEven = useMemo(() => {
+        console.warn(".......");
+        let i = 0;
+        while(i < 2000000000) {
+            i++;
+        }
+        return counterOne % 2 === 0;
+    }, [counterOne]);
+
+    return (
+        <div>
+            <button onClick={incrementOne}>Counter One - {counterOne}</button>
+            <span>{isEven?"Even": "Odd"}</span>
+            <button onClick={incrementTwo}>Counter Two - {counterTwo}</button>
+        </div>
+    );
+}
+
+
+export default Memo;
